e37655f826e8ab92a5e88a25c3c39c7029afb897
Adil Prima <adiru.prima@gmail.com>
2024-09-08 19:26:55 +0700

    Update forgotten functions

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,3 +1,65 @@
+function getImageDataURL(file, callback) {
+  const reader = new FileReader();
+  reader.onloadend = () => callback(reader.result);
+  reader.readAsDataURL(file);
+}
+
+function addProject(event) {
+  event.preventDefault();
+
+  const projectName = document.getElementById("project-name").value;
+  const projectStartDate = document.getElementById("start-date").value;
+  const projectEndDate = document.getElementById("end-date").value;
+  const projectDescription = document.getElementById("description").value;
+  const projectTech = document.querySelectorAll("input[name='tech']:checked");
+  const projectImage = document.getElementById("imageUpload").files[0];
+
+  getImageDataURL(projectImage, (imageDataURL) => {
+    // Calculate project duration
+    const startDateInMs = new Date(projectStartDate);
+    const endDateInMs = new Date(projectEndDate);
+    let projectDurationNum = endDateInMs - startDateInMs;
+    projectDurationNum = projectDurationNum / (1000 * 60 * 60 * 24); // Convert ms to days
+    let projectDuration = "";
+    if (projectDurationNum / 365 >= 1) {
+      const years = Math.floor(projectDurationNum / 365);
+      projectDuration += years + " years ";
+      projectDurationNum -= 365 * years;
+    }
+    if (projectDurationNum / 30 >= 1) {
+      const months = Math.floor(projectDurationNum / 30);
+      projectDuration += months + " months ";
+      projectDurationNum -= 30 * months;
+    }
+    projectDuration += Math.floor(projectDurationNum) + " days";
+
+    // Get project technologies used
+    const projectTechArray = [];
+    projectTech.forEach((tech) => {
+      projectTechArray.push(tech.value);
+    });
+
+    const data = {
+      title: projectName,
+      duration: projectDuration,
+      details: projectDescription,
+      tools: projectTechArray,
+      image: imageDataURL, // Use data URL instead of Blob URL
+    };
+
+    // Retrieve existing projects from localStorage or initialize as empty array
+    const existingProjects = JSON.parse(localStorage.getItem("projects")) || [];
+    existingProjects.push(data);
+
+    // Save the updated projects array to localStorage
+    localStorage.setItem("projects", JSON.stringify(existingProjects));
+
+    // Redirect or clear form
+    document.getElementById("postForm").reset(); // Reset form fields if desired
+    window.location.href = "index.html"; // Redirect to display page (index.html)
+  });
+}
+
 function renderProjects() {
   // Retrieve projects from localStorage
   const projects = JSON.parse(localStorage.getItem("projects")) || [];
